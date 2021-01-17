c0fb57cec6e8d73b1d552f034822074453759cec
Kaushal Sali <salikaushal@gmail.com>
2021-01-17 15:37:45 -0500

    Ports added to the interface.

--- src/RaagSequencer.cpp
+++ src/RaagSequencer.cpp
@@ -19,4 +19,21 @@ RaagSequencerWidget::RaagSequencerWidget(RaagSequencer* module) {
     addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
     addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
     addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+    for (int i=0; i<12; i++) {
+        // Aroha
+        addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 10 + i * 10)), module, RaagSequencer::IN_AROHA_SA + i));
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(35.24, 10 + i * 10)), module, RaagSequencer::OUT_AROHA_SA + i));
+        // Avroha
+        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(150 - 35.24, 9.5 + i * 10)), module, RaagSequencer::OUT_AVROHA_SA + i));
+        addInput(createInputCentered<PJ301MPort>(mm2px(Vec(150 - 15.24, 10 + i * 10)), module, RaagSequencer::IN_AVROHA_SA + i));
+    }
+
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(150.f/2, 10 + 2 * 10)), module, RaagSequencer::IN_TRIGGER));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(150/2, 10 + 4 * 10)), module, RaagSequencer::IN_GATE));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(150/2, 10 + 6 * 10)), module, RaagSequencer::IN_DIRECTION));
+    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(150/2, 10 + 8 * 10)), module, RaagSequencer::OUT_VOCT));
+
+    //addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 46.063)), module, RaagSequencer::PITCH_PARAM));
+    //addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(15.24, 25.81)), module, RaagSequencer::BLINK_LIGHT));
 }
--- src/RaagSequencer.h
+++ src/RaagSequencer.h
@@ -3,24 +3,57 @@
 //
 
 #include "plugin.hpp"
+#include "RaagEngine.h"
+#include <array>
 
 
 struct RaagSequencer : Module {
+public:
     enum ParamIds {
         NUM_PARAMS
     };
     enum InputIds {
+        IN_AROHA_SA,
+        IN_AVROHA_SA = IN_AROHA_SA + 12,
+        IN_TRIGGER = IN_AVROHA_SA + 12,
+        IN_DIRECTION,
+        IN_GATE,
         NUM_INPUTS
     };
     enum OutputIds {
+        OUT_AROHA_SA,
+        OUT_AVROHA_SA = IN_AROHA_SA + 12,
+        OUT_VOCT = IN_AVROHA_SA + 12,
         NUM_OUTPUTS
     };
     enum LightIds {
         NUM_LIGHTS
     };
 
+    static float freqToVolt(float frequencyInHz, bool isLFO=false) {
+        return std::log2(frequencyInHz/ (isLFO ? 2.0f : dsp::FREQ_C4));
+    }
+
+    static float voltToFreq(float frequencyInHz, bool isLFO=false) {
+        return std::log2(frequencyInHz/ (isLFO ? 2.0f : dsp::FREQ_C4));
+    }
+
+    static double midiToFreq(int midiNoteNumber) {
+        return std::pow(2, static_cast<double>(midiNoteNumber)/12) * 440;
+    }
+
     RaagSequencer();
     void process(const ProcessArgs& args) override;
+
+private:
+    void updateConnections();
+
+    RaagEngine m_raagEngine;
+    dsp::SchmittTrigger m_trigger;
+    std::array<float, static_cast<int>(Note::TOTAL)> m_arohaInputLastValues;
+    std::array<float, static_cast<int>(Note::TOTAL)> m_avrohaInputLastValues;
+    bool m_isFirstStep = true;
+
 };
 
 
